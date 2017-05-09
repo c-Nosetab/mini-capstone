b889549396cd06ee42700316003725a5bce5b369
c-Nosetab <cpbateson@gmail.com>
2017-05-08 21:34:01 -0500

    Added restful notation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,53 @@
 class ProductsController < ApplicationController
 
-  def products
-    @items = Product.all
-    render 'productlist.html.erb'
+  def index
+    @cookies = Product.all
   end
+
+  def show
+    cookie_id = params[:id]
+    @cookie = Product.find_by(id: cookie_id)
+  end
+
+  def new
+  end
+
+  def create
+    cookie = Product.new(
+                        name: params[:name],
+                        price: params[:price],
+                        image: params[:image],
+                        description: params[:description]
+                        )
+    cookie.save
+
+    flash[:success] = "Product Added"
+    redirect_to "/cookies/#{cookie.id}"
+  end
+
+  def edit
+    @cookie = Product.find(params[:id])
+  end
+
+  def update
+    cookie = Product.find(params[:id])
+    cookie.assign_attributes(
+                                name: params[:name],
+                                price: params[:price],
+                                image: params[:image],
+                                description: params[:description]
+                                )
+    cookie.save
+
+    flash[:success] = "Item updated"
+    redirect_to "/cookies/#{cookie.id}"
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    flash[:warning] = "Successfully Deleted"
+    redirect_to '/'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  get '/products' => 'products#products'
+  get '/' => 'products#index'
+  get '/cookies' => 'products#index'
+  get '/cookies/new' => 'products#new'
+  post '/cookies' => 'products#create'
+  get '/cookies/:id' => 'products#show'
+  get '/cookies/:id/edit' => 'products#edit'
+  patch '/cookies/:id' => 'products#update'
+  delete '/cookies/:id' => 'products#destroy'
+
+
+
+
 end
