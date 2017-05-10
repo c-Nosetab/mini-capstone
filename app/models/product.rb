88a02c5af08466303ea67a2e359db57a9ceaa36f
c-Nosetab <cpbateson@gmail.com>
2017-05-09 21:29:14 -0500

    added some cool css features, plus methods in the model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,25 @@
 class Product < ApplicationRecord
+
+  def friendly_info
+    "#{name}: $#{price}.00"
+  end
+
+  def friendly_time
+    updated_at.strftime("%A, %b %d %Y")
+  end
+
+  def discount_item
+    if price < 2
+      message = "#{price}.99 - Discount Item!"
+    else
+      message = "#{price}.99 - Everyday Value!"
+    end
+    message
+  end
+
+  def total_price
+    total = price + (price* 0.09)
+
+  end
+
 end
